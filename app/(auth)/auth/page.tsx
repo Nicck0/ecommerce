cfb77c88714cafa7684e3dc8c9fc81689546bd5e
Nicholas <gauchodev0ps@gmail.com>
2025-07-08 00:48:00 +0300

    sign in n up  pages ui

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -14,14 +14,37 @@ export default function AuthPage() {
   return (
     <div className='flex min-h-screen mt-7'>
         <div className="w-full max-w-3xl mx-auto flex flex-col lg: flex-row p-3">
-            <div
-             className="mb-8 lg:mb-12 cursor-pointer"
-            onClick={()=> router.push('/')}
-            >
+            <div>
+                <div
+                     className="mb-8 lg:mb-12 cursor-pointer"
+                     onClick={()=> router.push('/')}
+                        >
                 <ChevronLeft className='text-gray-500 h-6 w-6 sm: h-8 sm:w-8 border-2 rounded-full p-1'></ChevronLeft>
+                </div>
+                 <div>
+                    <h2 className='text-3xl sm:text-4xl lg: text-5xl font-bold mb-4 sm: mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3'>
+                    {isSignUp ? 'Sign Up' : 'Sign In'}
+                    </h2>
+
+                    <p className='text-base sm: text-lg lg: text-xl text-gray-400 mb-6 sm:mb-8'>
+                    {isSignUp ?
+                    'join Nicholas today and discover exclusive deals on your favourite products!':
+                    'welcome back to NicholasStore! Log in to continue your shopping journey'}
+                     </p>
+                     </div>
+                     <div className="mt-4 sm:mt-5 flex items-center justify-center">
+                         <p className="text-base sm:text-lg lg:text-xl text-gray-600">
+                         {isSignUp ? 'Already a member?': "Don't have an account?"}
+                          </p>
+                          <button variant='link'
+                             className='text-lg sm:text-xl lg:text-2xl text-gray-500 cursor-pointer'
+                                onClick={tougleForm}>
+                                 {isSignUp ? 'Sign in' : 'Sign up'}
+                           </button>
+            
+                     </div>
             </div>
         </div>
-      AuthPage
     </div>
   )
 }
